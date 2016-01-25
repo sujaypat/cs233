ae19c3d6d0e84293ad13e673fe11de49ddfae6d4
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-01-25 20:54:02 +0000

    sc2_block finished

--- ptwrdhn2/Lab1/sc2_block.v
+++ ptwrdhn2/Lab1/sc2_block.v
@@ -2,5 +2,12 @@
 // Don't put any code in this file besides the sc2_block
 
 module sc2_block(s, cout, a, b, cin);
+   output s, cout;
+   input  a, b, cin;
+   wire   s1, c1, c2;
 
+   sc_block b1(s1, c1, a, b);
+   sc_block b2(s, c2, s1, cin);
+   or o1(cout, c1, c2);
+   
 endmodule // sc2_block
--- ptwrdhn2/Lab1/sc2_block_tb.v
+++ ptwrdhn2/Lab1/sc2_block_tb.v
@@ -1,3 +1,74 @@
 module sc2_test;
+   reg a_in, b_in, cin_in;
+                              // these are inputs to "circuit under test"
+                                                 // use "reg" not "wire" so can assign a value
+   wire s_out, cout_out;
+                           // wires for the outputs of "circuit under test"
 
+   sc2_block sc2 (s_out, cout_out, a_in, b_in, cin_in);
+     // the circuit under test
+
+       initial begin                             // initial = run at beginning of simulation
+	                                                // begin/end = associate block with initial
+
+	  $dumpfile("sc2.vcd");
+	                    // name of dump file to create
+	  $dumpvars(0, sc2_test);
+	                   // record all signals of module "sc_test" and sub-modules
+	                                                // remember to change "sc_test" to the correct
+	                                                // module name when writing your own test benches
+
+	          // test all four input combinations
+	          // remember that 2 inputs means 2^2 = 4 combinations
+	          // 3 inputs would mean 2^3 = 8 combinations to test, and so on
+	          // this is very similar to the input columns of a truth table
+	  a_in = 0;
+	  b_in = 0;
+	  cin_in = 0;
+	  # 10;
+	               // set initial values and wait 10 time units
+	  a_in = 0;
+	  b_in = 0;
+	  cin_in = 1;
+	  # 10;
+	               // change inputs and then wait 10 time units
+	  a_in = 0;
+	  b_in = 1;
+	  cin_in = 0;
+	  # 10;
+	               // as above
+	  a_in = 0;
+	  b_in = 1;
+	  cin_in = 1;
+	  # 10;
+
+	  a_in = 1;
+	  b_in = 0;
+	  cin_in = 0;
+	  # 10;
+	               // set initial values and wait 10 time units
+	  a_in = 1;
+	  b_in = 0;
+	  cin_in = 1;
+	  # 10;
+	               // change inputs and then wait 10 time units
+	  a_in = 1;
+	  b_in = 1;
+	  cin_in = 0;
+	  # 10;
+	               // as above
+	  a_in = 1;
+	  b_in = 1;
+	  cin_in = 1;
+	  # 10;
+
+
+	  $finish;
+	                                // end the simulation
+       end // initial begin
+
+       initial
+	         $monitor("At time %2t, a_in = %d b_in = %d cin = %d s_out = %d cout = %d",
+			  $time, a_in, b_in, cin_in, s_out, cout_out);
+   
 endmodule // sc2_test
