1c9ab26be79a26b502bb14c96d3a483b641a4a3e
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-01-27 18:39:53 +0000

    keypad

--- ptwrdhn2/Lab2/countOnes.cpp
+++ ptwrdhn2/Lab2/countOnes.cpp
@@ -2,9 +2,17 @@
  * @file
  * Contains the implementation of the countOnes function.
  */
-
+#include <iostream>
+using namespace std;
 unsigned countOnes(unsigned input) {
 	// TODO: write your code here
+
+  unsigned oddCounters = input & 0x5555;
+  unsigned evenCounters = input & 0xAAAA;
+
+  unsigned pairAndAdded = (evenCounters >> 1) + oddCounters;
+
+  cout << pairAndAdded << endl;
 
 	
 
