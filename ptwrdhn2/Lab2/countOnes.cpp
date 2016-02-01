dbbcfe6d1859a203430780302adebc85ae7f5ad3
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-02-01 01:29:29 +0000

    should be working

--- ptwrdhn2/Lab2/countOnes.cpp
+++ ptwrdhn2/Lab2/countOnes.cpp
@@ -7,14 +7,36 @@ using namespace std;
 unsigned countOnes(unsigned input) {
 	// TODO: write your code here
 
-  unsigned oddCounters = input & 0x5555;
-  unsigned evenCounters = input & 0xAAAA;
+  unsigned int odd = input & 0x55555555;
+  unsigned int even = input & 0xAAAAAAAA;
+    
+  even >>= 1;
+  input = odd + even;
+    
+  //alternate in twos
+  odd = input & 0x33333333;
+  even = input & 0xCCCCCCCC;
+  even >>= 2;
+  input = odd + even;
+    
+  //alternate in fours
+  odd = input & 0x0F0F0F0F;
+  even = input & 0xF0F0F0F0;
+  even >>= 4;
+  input = odd + even;
+    
+  //alternate in eights
+  odd = input & 0x00FF00FF;
+  even = input & 0xFF00FF00;
+  even >>= 8;
+  input = odd + even;
+    
+  //alternate in 16s
+  odd = input & 0x0000FFFF;
+  even = input & 0xFFFF0000;
+  even >>= 16;
+  input = odd + even;
+    
+  return input;
 
-  unsigned pairAndAdded = (evenCounters >> 1) + oddCounters;
-
-  cout << pairAndAdded << endl;
-
-	
-
-	return input;
 }
