af8da23a0bc4021f13441f45756572531c8fcb0d
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-02-16 04:54:44 +0000

    y no spim?

--- ptwrdhn2/Lab5/arith_machine.v
+++ ptwrdhn2/Lab5/arith_machine.v
@@ -8,21 +8,47 @@ module arith_machine(except, clock, reset);
     output      except;
     input       clock, reset;
 
-    wire [31:0] inst;  
-    wire [31:0] PC;  
+    wire [31:0] inst;
+    wire [31:0] PC;
+	wire [31:0] PC_next;
+	wire [31:0] rdData;
+	wire [31:0] A;
+	wire [31:0] B;
+	wire [4:0]  Rdest;
+	wire [31:0] rtData;
+	wire [31:0] rsData;
+	wire [31:0] imm32;
+	wire [2:0]  alu_op;
+	wire 		writeenable;
+	wire 		rd_src;
+	wire 		alu_src2;
+	wire 		overflow, zero, negative;
+    // DO NOT comment out or rename this module
+    // or the test bench will break
+    register #(32) PC_reg(PC, PC_next, clock, 1'b1, reset);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    register #(, 32) PC_reg( /* connect signals */ );
+    instruction_memory im(inst[31:0], PC[31:2]);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+    regfile rf (rsData, rtData, inst[25:21], inst[20:16], Rdest, rdData, writeenable, clock, reset);
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    regfile rf ( /* connect signals */ );
+	alu32 a1(PC_next,,,, PC[31:0], 32'h4, `ALU_ADD);
+	alu32 a2(rdData, overflow, zero, negative, A, B, alu_op);
+	mux2v #(5) m1(Rdest, inst[15:11], inst[20:16], rd_src);
+	mux2v m2(B, rtData, imm32, alu_src2);
+	sign_extender s1(imm32, inst[15:0]);
+	mips_decode md1(alu_op, writeenable, rd_src, alu_src2, except, inst[31:26], inst[5:0]);
 
     /* add other modules */
-   
+
 endmodule // arith_machine
+
+module sign_extender (imm, inst);
+    output [31:0] imm;
+    input [15:0] inst;
+    assign imm = {{16{inst[15]}}, inst[15:0]};
+
+endmodule
--- ptwrdhn2/Lab5/decoder_tb.v
+++ ptwrdhn2/Lab5/decoder_tb.v
@@ -8,7 +8,20 @@ module decoder_test;
              opcode = `OP_OTHER0; funct = `OP0_ADD; // try addition
         # 10 opcode = `OP_OTHER0; funct = `OP0_SUB; // try subtraction
         // add more tests here!
-
+				# 10 opcode = `OP_OTHER0; funct = `OP0_AND;
+	      # 10 opcode = `OP_OTHER0; funct = `OP0_OR;
+	      # 10 opcode = `OP_OTHER0; funct = `OP0_NOR;
+	      # 10 opcode = `OP_OTHER0; funct = `OP0_XOR;
+	      # 10 opcode = `OP_ADDI;
+	      # 10 opcode = `OP_ANDI;
+	      # 10 opcode = `OP_ORI;
+	      # 10 opcode = `OP_XORI;
+	      # 10 opcode = 6'b101010; funct = `OP0_ADD;
+	      # 10 opcode = 6'b110111; funct = `OP0_SUB;
+	      # 10 opcode = 6'b110111; funct = `OP0_AND;
+	      # 10 opcode = 6'b110111; funct = `OP0_OR;
+	      # 10 opcode = 6'b110111; funct = `OP0_NOR;
+	      # 10 opcode = 6'b110111; funct = `OP0_XOR;
         # 10 $finish;
     end
 
