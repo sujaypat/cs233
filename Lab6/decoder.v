f10fcafa526f82cf4a55511bcf23ee591fb7bf32
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-02-24 21:47:04 +0000

    ebola

--- Lab6/decoder.v
+++ Lab6/decoder.v
@@ -51,6 +51,7 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
 	assign op_lbu = (opcode == `OP_LBU);
 	assign op_sw = (opcode == `OP_SW);
 	assign op_sb = (opcode == `OP_SB);
+	assign op_addm = (opcode == `OP_OTHER0) & (funct == `OP0_ADDM);
 
 	assign alu_op[0] = (op_sub | op_or | op_xor | op_ori | op_xori | op_beq | op_bne | op_slt);
     assign alu_op[1] = (op_add | op_sub| op_nor | op_xor | op_addi | op_xori | op_beq | op_bne | op_slt | op_lw | op_lbu | op_sw | op_sb);
@@ -66,6 +67,7 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
     assign byte_load = op_lbu;
     assign lui = op_lui;
     assign slt = op_slt;
+	assign addm = op_addm;
 
     assign except = ~(op_add | op_sub | op_and | op_or | op_nor | op_xor | op_addi | op_andi | op_ori | op_xori | op_beq | op_bne | op_j | op_jr | op_lui | op_slt | op_lw | op_lbu | op_sw | op_sb);
 
