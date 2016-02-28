718334f6965f8c21a65fd4260b4747cb94cb385e
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-02-28 01:12:13 +0000

    fuck this shit

--- ptwrdhn2/Lab6/fm.s
+++ ptwrdhn2/Lab6/fm.s
@@ -4,10 +4,25 @@
 .data
 # your test data goes here
 a: .word 0 1 2 4 8 16
+array:	.word	1	255	1024
 
 .text
-main:
-	la $4, a
-	lw $5, 0($4)
-	lw $6, 4($4)
-	
+# main:
+# 	la	$4, a
+# 	lw	$5, 0($4)
+# 	lw	$6, 4($4)
+# 	lui $29, 14687
+main:   addi	$10, $0, 255		# $10 = 255
+	la	$2, array   		# $2  = 0x10010000
+	lw	$3, 0($2)		# $3  = 1
+	lw	$4, 4($2)		# $4  = 255
+	beq	$4, $10, equal		# branch taken
+
+	addi	$11, $0, 1		# skipped
+	j	end	    		# skipped
+
+equal:	addi	$12, $0, 2		# $12 = 2
+	bne	$4, $10, end		# branch not taken
+	add	$14, $3, $4		# $14 = 256
+
+end:	addi	$13, $0, 3		# $13 = 3
