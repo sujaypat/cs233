0fa6a2f8092cbd9d946be1a4fe5836be5bec098f
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-03-01 18:51:23 +0000

    FUCKING SVN BROKE

--- ptwrdhn2/Lab7/length.s
+++ ptwrdhn2/Lab7/length.s
@@ -14,4 +14,18 @@
 .globl length
 length:
 	# Your code goes here :)
+	li		$t1, 0		# num_bits
+	move	$t2, $a0	# argument
+
+forloop:
+	ble		$t2, 0, endfor
+	bge		$t1, 32, endfor
+	srl		$t2, $t2, 1
+	add		$t1, $t1, 1
+	j		forloop
+
+
+endfor:
+	move 	$v0, $t1
+
 	jr	$ra
--- ptwrdhn2/Lab7/str_cmp.s
+++ ptwrdhn2/Lab7/str_cmp.s
@@ -14,4 +14,27 @@
 .globl str_cmp
 str_cmp:
 	# Your code goes here :)
+	move 	$t0, $a0 # copy of src
+	move 	$t1, $a1 # copy of tgt
+
+forloop:
+	lbu		$s0, 0($t0)		#
+	lbu		$s1, 0($t1)		#
+	and		$t2, $s0, $s1 	# and statement
+	beq		$t2, 0, end		# check that *src && *tgt is not 0
+	bne		$s0, $s1, end	# compare *src and *tgt
+	add		$t0, $t0, 1		# src++
+	add		$t1, $t1, 1		# tgt++
+	j		forloop			# jump to beginning of loop
+
+end:
+	bne		$s0, $s1, endfalse	# check if src = tgt
+	li 		$v0, 0				# return false
+	j 		quit
+
+endfalse:
+	li		$v0, 1				# return true
+
+quit:
+
 	jr	$ra
