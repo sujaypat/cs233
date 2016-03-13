b42bf60870a44a72d17b77cf625e78eb8764aec0
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-03-13 19:02:58 +0000

    mips = ebola

--- Lab8/split_string.s
+++ Lab8/split_string.s
@@ -1,5 +1,4 @@
 .text
-
 ## int
 ## split_string(char **solution, char *str, const dictionary *dict) {
 ##     *solution = NULL;
@@ -22,9 +21,78 @@
 ##     return 0;
 ## }
 
-
 .globl split_string
 split_string:
 	# Your code goes here :)
-	jr	$ra
+	sub		$sp, $sp, 28	# sub sp
+	sw		$ra, 0($sp)		# store ra in memory
+	sw		$a0, 4($sp)		# store **solution in memory
+	sw		$a1, 8($sp)		# store *str in memory
+	sw		$a2, 12($sp)	# store *dict
 
+	sw		$0, 0($a0)		# *solution = NULL
+	move 	$t0, $a1		# *ptr = str
+	sw		$t0, 16($sp)	# store *ptr in memory
+
+	lbu		$t1, 0($t0)		# dereference *ptr
+	sb		$t1, 20($sp)	# store ptr value in memory
+
+	beq		$t1, $0, return1# if *ptr == 0 return 1
+
+forloop:
+	beq		$t1, $0, return0# if *ptr == 0 break
+
+	move 	$a0, $a1		# load str into arg0
+	add		$t0, $t0, 1		# ptr + 1
+	sub		$t0, $t0, $a0	# ptr + 1 - str
+	move 	$a1, $t0		# move prt + 1 - str to arg1
+	jal		sub_str			# sub_str(str, ptr + 1 - str)
+	move 	$t2, $v0		# save prefix to register
+	sw		$t2, 24($sp)	# store prefix in memory
+
+	move 	$a0, $t2		# copy prefix to arg0
+	lw		$t1, 12($sp)	# load dict from memory
+	lw		$a2, 0($t1)		# load dict->size into arg2
+	add		$a1, $t1, 4		# load dict->words into arg1
+	jal		in_dict			# in_dict(prefix, dict->words, dict->size)
+	beq		$v0, $0, continue# if result is 0 then continue loop
+
+	lw		$a0, 4($sp)		# load solution from memory
+	add		$a0, $a0, 4		# solution + 1 into arg0
+	lw		$a1, 16($sp)	# load ptr
+	add		$a1, $a1, 1		# ptr + 1 into arg1
+	lw		$a2, 12($sp)	# load dict into arg2
+	jal		split_string	# recursive call split_string(solution + 1, ptr + 1, dict)
+	beq		$v0, $0, continue# if result is 0 then continue loop
+
+	lw		$t3, 4($sp)		# *solution
+	lw		$t2, 24($sp)	# load prefix from memory
+	sw 		$t2, 0($t3)		# *solution = prefix
+
+	j		return1			# jump to return0
+
+continue:
+	lw		$t0, 16($sp)	# load ptr from memory
+	add		$t0, $t0, 1		# ptr++
+	sw		$t0, 16($sp)	# store ptr back into memory
+	lw		$a0, 4($sp)		# load **solution from memory
+	lw		$a1, 8($sp)		# load *str from memory
+	lw		$a2, 12($sp)	# load *dict
+	lbu		$t1, 0($t0)		# load *ptr
+
+	j 		forloop			# loop
+
+return1:
+	li		$v0, 1			# load 1 into return value
+	j		end				# jump to end
+
+return0:
+	li		$v0, 0			# load 0 into return value
+
+end:
+	lw	$ra, 0($sp)			# load ra from memory
+	lw	$a2, 12($sp)		# a2 is const
+
+	add	$sp, $sp, 28		# add to sp
+
+	jr	$ra
--- Lab8/sub_str.s
+++ Lab8/sub_str.s
@@ -11,40 +11,78 @@
 ##     return newstr;
 ## }
 
+# .globl sub_str
+# sub_str:
+# 	# Your code goes here :)
+# 	sub		$sp, $sp, 12	# subtract sp
+# 	sw		$a0, 0($sp)		# store char* str
+# 	sw		$a1, 4($sp)		# store n
+# 	sw		$ra, 8($sp)		# store ra
+#
+# 	add		$t2, $a1, 1		# n + 1
+#
+# 	move 	$a0, $t2		# copy n - 1 into argument0
+# 	jal		malloc			# call malloc on n - 1
+# 	move 	$t9, $v0		# load result into newstr
+#
+# 	lw		$a0, 0($sp)		# reload char* str after malloc
+# 	lw		$a1, 4($sp)		# reload n after malloc
+# 	li		$t3, 0			# len = 0
+#
+# forloop:
+# 	bge		$t3, $a1, end	# if len >= n end loop
+# 	add		$t5, $a0, $t3	# offset = counter
+# 	lbu		$t4, 0($t5)		# load str[offset]
+# 	beq		$t4, $0, end	# if str[len] == 0 end loop
+# 	add		$t8, $t9, $t3	# offset = counter
+# 	sb		$t4, 0($t8)		# store str[offset] into newstr[offset]
+# 	add		$t3, $t3, 1		# len++
+# 	j		forloop			# jump to forloop
+#
+#
+#
+# end:
+# 	add		$a0, $t3, $t9	#  =  +
+# 	sb		$0, 0($a0)		# store null in last entry of array
+# 	move 	$v0, $t9		# store newstr into return reg
+# 	lw		$ra, 8($sp)		# reload ra
+# 	add		$sp, $sp, 12	# put back sp
+#     jr  	$ra
+
+
+
 .globl sub_str
 sub_str:
-	# Your code goes here :)
-	sub		$sp, $sp, 12	# subtract sp
-	sw		$a0, 0($sp)		# store char* str
-	sw		$a1, 4($sp)		# store n
-	sw		$ra, 8($sp)		# store ra
+	sub	$sp, $sp, 12
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
 
-	add		$t2, $a1, 1		# n + 1
+	move	$s0, $a0		# str
+	move	$s1, $a1		# n
 
-	move 	$a0, $t2		# copy n - 1 into argument0
-	jal		malloc			# call malloc on n - 1
-	move 	$t9, $v0		# load result into newstr
+	add	$a0, $s1, 1		# n + 1
+	jal	malloc			# malloc(n + 1)
+	li	$t0, 0			# len = 0
 
-	lw		$a0, 0($sp)		# reload char* str after malloc
-	lw		$a1, 4($sp)		# reload n after malloc
-	li		$t3, 0			# len = 0
+sub_str_for:
+	bge	$t0, $s1, sub_str_ret	# len >= n
+	add	$t1, $s0, $t0		# &str[len]
+	lb	$t1, 0($t1)		# str[len]
+	beq	$t1, 0, sub_str_ret	# str[len] == '\0'
 
-forloop:
-	bge		$t3, $a1, end	# if len >= n end loop
-	add		$t5, $a0, $t3	# offset = counter
-	lbu		$t4, 0($t5)		# load str[offset]
-	beq		$t4, $0, end	# if str[len] == 0 end loop
-	add		$t8, $t9, $t3	# offset = counter
-	sb		$t4, 0($t8)		# store str[offset] into newstr[offset]
-	add		$t3, $t3, 1		# len++
-	j		forloop			# jump to forloop
+	add	$t2, $v0, $t0		# &newstr[len]
+	sb	$t1, 0($t2)		# newstr[len] = str[len]
 
+	add	$t0, $t0, 1		# len++
+	j	sub_str_for
 
+sub_str_ret:
+	add	$t2, $v0, $t0		# &newstr[len]
+	sb	$0, 0($t2)		# newstr[len] = '\0'
 
-end:
-	add		$a0, $t3, $t9	#  =  +
-	sb		$0, 0($a0)		# store null in last entry of array
-	move 	$v0, $t9		# store newstr into return reg
-	lw		$ra, 8($sp)		# reload ra
-	add		$sp, $sp, 12	# put back sp
-    jr  	$ra
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	add	$sp, $sp, 12
+	jr	$ra
