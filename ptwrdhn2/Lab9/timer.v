ab3a9497571d6cb944e9a6ca143caeb3619647e0
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-03-14 20:51:03 +0000

    lab 9.1 submit

--- ptwrdhn2/Lab9/timer.v
+++ ptwrdhn2/Lab9/timer.v
@@ -5,7 +5,32 @@ module timer(TimerInterrupt, TimerAddress, cycle,
     input  [31:0] address, data;
     input         MemRead, MemWrite, clock, reset;
 
+	wire [31:0]   Qcyc, Dcyc, Qint, Dint;
+	wire 		  Qline;
+
+
+	wire one_c_equals_address = (32'hffff001c == address);
+	wire six_c_equals_address = (32'hffff006c == address);
+
+
+	wire TimerWrite = MemWrite && one_c_equals_address;
+	wire TimerRead = MemRead && one_c_equals_address;
+	wire Acknowledge = MemWrite && six_c_equals_address;
+
+	assign TimerAddress = (one_c_equals_address || six_c_equals_address);
+	assign TimerInterrupt = Qline;
+
     // complete the timer circuit here
+
+	register #(32) cycle_counter(Qcyc, Dcyc, clock, 1'b1, reset);
+
+	alu32 a1(Dcyc, , , `ALU_ADD, Qcyc, 32'b1);
+
+	register #(32, 32'hffffffff) interrupt_cycle(Qint, Dint, clock, TimerWrite, reset);
+
+	dffe #(1) interrupt_line(Qline, 1'b1, clock, (Qcyc == Qint), Acknowledge || reset);
+
+	tristate same(cycle, Qcyc, TimerRead);
 
     // HINT: make your interrupt cycle register reset to 32'hffffffff
     //       (using the reset_value parameter)
