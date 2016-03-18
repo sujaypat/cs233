fa5f47205951663c6a26f66ae3d6ce239c30792f
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-03-18 02:43:59 +0000

    you played youself

--- ptwrdhn2/Lab9/cp0.v
+++ ptwrdhn2/Lab9/cp0.v
@@ -13,5 +13,46 @@ module cp0(rd_data, EPC, TakenInterrupt,
     input  [29:0] next_pc;
     input         TimerInterrupt, MTC0, ERET, clock, reset;
 
+	wire   [29:0] Q, d_epc;
+	wire   [31:0] mtc0_out, status_register, cause_register, user_status;
+	wire          exception_level;
+
+	assign status_register[31:16] = 0;
+	assign status_register[15] =  user_status[15];
+	assign status_register[14] =  user_status[14];
+	assign status_register[13] =  user_status[13];
+	assign status_register[12] =  user_status[12];
+	assign status_register[11] =  user_status[11];
+	assign status_register[10] =  user_status[10];
+	assign status_register[9] =  user_status[9];
+	assign status_register[8] =  user_status[8];
+	assign status_register[7:2] = 0;
+	assign status_register[1] = exception_level;
+	assign status_register[0] = user_status[0];
+
+	assign cause_register[31:16] = 0;
+	assign cause_register[15] = TimerInterrupt;
+	assign cause_register[14:0] = 0;
+	assign EPC = Q;
+	// assign rd_data = 32'b0;
+
+
+
     // your Verilog for coprocessor 0 goes here
+
+	decoder32 mtc0(mtc0_out, regnum, MTC0);
+
+	mux32v #(32) rd_out(rd_data, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, status_register, cause_register, {Q[29:0], 2'b0}, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, regnum);
+
+	dffe dffe_exception_level(exception_level, 1'b1, clock, TakenInterrupt, reset || ERET);
+
+	register reg_user_status(user_status, wr_data, clock, (mtc0_out[12] && (regnum == `STATUS_REGISTER)), reset);
+
+	mux2v #(30) data_or_next(d_epc, wr_data[31:2], next_pc, TakenInterrupt);
+
+	register #(30) epc(Q, d_epc, clock, TakenInterrupt || mtc0_out[14], reset);
+
+	assign TakenInterrupt = ((cause_register[15] && status_register[15]) && (!status_register[1] && status_register[0]));
+
+
 endmodule
--- ptwrdhn2/Lab9/machine.v
+++ ptwrdhn2/Lab9/machine.v
@@ -1,45 +1,53 @@
 module machine(clk, reset);
-   input        clk, reset;
+	input        clk, reset;
 
-   wire [31:0]  PC;
-   wire [31:2]  next_PC, PC_plus4, PC_target;
-   wire [31:0]  inst;
-   
-   wire [31:0]  imm = {{ 16{inst[15]} }, inst[15:0] };  // sign-extended immediate
-   wire [4:0]   rs = inst[25:21];
-   wire [4:0]   rt = inst[20:16];
-   wire [4:0]   rd = inst[15:11];
+	wire [31:0]  PC;
+	wire [31:2]  next_PC, PC_plus4, PC_target, next_PC_new;
+	wire [31:0]  inst;
 
-   wire [4:0]   wr_regnum;
-   wire [2:0]   ALUOp;
+	wire [31:0]  imm = {{ 16{inst[15]} }, inst[15:0] };  // sign-extended immediate
+	wire [4:0]   rs = inst[25:21];
+	wire [4:0]   rt = inst[20:16];
+	wire [4:0]   rd = inst[15:11];
 
-   wire         RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET;
-   wire         PCSrc, zero, negative;
-   wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data;
+	wire [4:0]   wr_regnum;
+	wire [2:0]   ALUOp;
+
+	wire         RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET;
+	wire         PCSrc, zero, negative, NotIO, TakenInterrupt, TimerAddress, TimerInterrupt;
+	wire [31:0]  rd1_data, rd2_data, B_data, alu_out_data, load_data, wr_data, cycle, address, data, c0_rd_data;
+	wire [31:2]  EPC, interrupt_taken;
 
 
-   register #(30, 30'h100000) PC_reg(PC[31:2], next_PC[31:2], clk, /* enable */1'b1, reset);
-   assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
-   adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
-   adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
-   mux2v #(30) branch_mux(next_PC, PC_plus4, PC_target, PCSrc);
-   assign PCSrc = BEQ & zero;
-      
-   instruction_memory imem (inst, PC[31:2]);
+	register #(30, 30'h100000) PC_reg(PC[31:2], next_PC_new[31:2], clk, /* enable */1'b1, reset);
+	assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
+	adder30 next_PC_adder(PC_plus4, PC[31:2], 30'h1);
+	adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
+	mux2v #(30) branch_mux(next_PC, PC_plus4, PC_target, PCSrc);
+	assign PCSrc = BEQ & zero;
+	assign NotIO = ~TimerAddress;
 
-   mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET,
-                      inst);
-   
-   regfile rf (rd1_data, rd2_data,
-               rs, rt, wr_regnum, wr_data, 
-               RegWrite, clk, reset);
+	mux2v #(30) eret_mux(interrupt_taken, next_PC, EPC, ERET);
 
-   mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
-   alu32 alu(alu_out_data, zero, negative, ALUOp, rd1_data, B_data);
-   
-   data_mem data_memory(load_data, alu_out_data, rd2_data, MemRead, MemWrite, clk, reset);
-   
-   mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg);
-   mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
-   
+	mux2v #(30) taken_interrupt_mux(next_PC_new, interrupt_taken, 30'h80000180, TakenInterrupt);
+
+	timer tim(TimerInterrupt, TimerAddress, cycle, address, data, MemRead, MemWrite, clk, reset);
+
+	cp0 copro0(c0_rd_data, EPC, TakenInterrupt, wr_regnum, wr_data, next_PC, TimerInterrupt, MTC0, ERET, clk, reset);
+
+	instruction_memory imem (inst, PC[31:2]);
+
+	mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, MFC0, MTC0, ERET, inst);
+
+	regfile rf (rd1_data, rd2_data, rs, rt, wr_regnum, wr_data, RegWrite, clk, reset);
+
+
+	mux2v #(32) imm_mux(B_data, rd2_data, imm, ALUSrc);
+	alu32 alu(alu_out_data, zero, negative, ALUOp, rd1_data, B_data);
+
+	data_mem data_memory(load_data, alu_out_data, rd2_data, MemRead && NotIO, MemWrite && NotIO, clk, reset);
+
+	mux2v #(32) wb_mux(wr_data, alu_out_data, load_data, MemToReg);
+	mux2v #(5) rd_mux(wr_regnum, rt, rd, RegDst);
+
 endmodule // machine
