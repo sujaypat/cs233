ce2dbc22f093486c42f88bd168885af07a314ae7
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-04-04 20:15:21 +0000

    ayy lmao

--- Lab11/pipelined_adding_machine_tb.v
+++ Lab11/pipelined_adding_machine_tb.v
@@ -1,14 +1,14 @@
 module test;
    /* Make a regular pulsing clock. */
    reg       clk = 0;
-   always #2 clk = !clk;
+   always #1 clk = !clk;
    integer     i;
    wire [31:0] out;
-   
+
    reg          reset = 1, done = 0;
 
    pipelined_adding_machine pam(out, clk, reset);
-   
+
    initial begin
       $dumpfile("pam.vcd");
       $dumpvars(0, test);
@@ -16,9 +16,9 @@ module test;
       # 50 done = 1;
       $finish;
    end
-   
+
   initial
      $monitor("At time %t, reset = %d index = %h, out = %h",
               $time, reset, pam.index, out);
-   
+
 endmodule // test
