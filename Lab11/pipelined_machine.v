8f29d03c4f4a69fca9bccff2fcb26172f06c6933
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-04-08 03:30:26 +0000

    forwarding implemented

--- Lab11/pipelined_machine.v
+++ Lab11/pipelined_machine.v
@@ -26,14 +26,12 @@ module pipelined_machine(clk, reset);
 	assign PC[1:0] = 2'b0;  // bottom bits hard coded to 00
 	adder30 next_PC_adder(PC_plus4_old, PC[31:2], 30'h1);
 	adder30 target_PC_adder(PC_target, PC_plus4, imm[29:0]);
-	mux2v #(30) branch_mux(next_PC, PC_plus4, PC_target, PCSrc);
+	mux2v #(30) branch_mux(next_PC, PC_plus4_old, PC_target, PCSrc);
 	assign PCSrc = BEQ & zero;
-
 
 	instruction_memory imem (inst_pre_pipe, PC[31:2]);
 	register #(32) if_de_imem(inst, inst_pre_pipe, clk, 1'b1, reset);
 	register #(30) if_de_pcplusfour(PC_plus4, PC_plus4_old, clk, 1'b1, reset);
-
 
 	mips_decode decode(ALUOp, RegWrite, BEQ, ALUSrc, MemRead, MemWrite, MemToReg, RegDst, opcode, funct);
 	register #(3) aluop_mw(ALUOp_MW, ALUOp, clk, 1'b1, reset);
@@ -44,7 +42,6 @@ module pipelined_machine(clk, reset);
 	register #(1) memwrite_mw(MemWrite_MW, MemWrite, clk, 1'b1, reset);
 	register #(1) memtoreg_mw(MemToReg_MW, MemToReg, clk, 1'b1, reset);
 	register #(1) regdst_mw(RegDst_MW, RegDst, clk, 1'b1, reset);
-
 
 	regfile rf (rd1_data_old, rd2_data, rs, rt, wr_regnum_MW, wr_data, RegWrite, clk, reset);
 
@@ -59,12 +56,10 @@ module pipelined_machine(clk, reset);
 	assign ForwardA = ((rs == wr_regnum_MW) && RegWrite_MW);
 	assign ForwardB = ((rt == wr_regnum_MW) && RegWrite_MW);
 	mux2v #(32) forwarda_mux(rd1_data, rd1_data_old, alu_out_data_MW, ForwardA);
-	mux2v #(32) forwardb_mux(rd2_data, B_data_old, alu_out_data_MW, ForwardB);
+	mux2v #(32) forwardb_mux(rd2_data_old, rd2_data, alu_out_data_MW, ForwardB);
 
 	register #(5) de_mw_regnum(wr_regnum_MW, wr_regnum, clk, 1'b1, reset);
 	register #(32) de_mw_forwardB(rd2_data_MW, rd2_data, clk, 1'b1, reset);
 	register #(32) de_mw_aluresult(alu_out_data_MW, alu_out_data, clk, 1'b1, reset);
-
-
 
 endmodule // pipelined_machine
