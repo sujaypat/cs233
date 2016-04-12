f0ae7c789343cef695b7171efc37f76ee56209d1
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-04-12 18:47:46 +0000

    lab 12 part 1 submit

--- ptwrdhn2/Lab12/simplecache.cpp
+++ ptwrdhn2/Lab12/simplecache.cpp
@@ -1,10 +1,27 @@
 #include "simplecache.h"
 
 int SimpleCache::find(int index, int tag, int block_offset) {
-  // read handout for implementation details
-  return 0xdeadbeef;
+	// read handout for implementation details
+	auto find = _cache.find(index);
+	for (int i = 0; i < _associativity; i++) {
+		auto look = find->second[i];
+		if(look.valid() && (look.tag() == tag)){
+			return look.get_byte(block_offset);
+		}
+	}
+	return 0xdeadbeef;
 }
 
 void SimpleCache::insert(int index, int tag, char data[]) {
-  // read handout for implementation details
+	// read handout for implementation details
+	auto find = _cache.find(index);
+	for (int i = 0; i < _associativity; i++) {
+		auto & look = find->second[i];
+		if(!look.valid()){
+			look.replace(tag, data);
+			return;
+		}
+	}
+	auto & look2 = find->second[0];
+	look2.replace(tag, data);
 }
