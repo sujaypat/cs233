479aafdd4cb3421092b9c130c271a1faae6d85ac
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-04-14 02:57:49 +0000

    may or may not be working, so call me maybe

--- ptwrdhn2/Lab12/cacheblock.cpp
+++ ptwrdhn2/Lab12/cacheblock.cpp
@@ -1,6 +1,11 @@
 #include "cacheblock.h"
 
 uint32_t Cache::Block::get_address() const {
-  // TODO
-  return 0;
+	// TODO
+	auto config = this->_cache_config;
+	uint32_t block_offset = 0;
+	uint32_t index = this->_index << config.get_num_block_offset_bits();
+	uint32_t tag = this->get_tag() << (config.get_num_index_bits() + config.get_num_block_offset_bits());
+	return (tag | index | block_offset);
+	// return 0;
 }
