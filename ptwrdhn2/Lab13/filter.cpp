01c7deb8542cfbae09f6ca954534af948ce07e7c
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-04-23 22:13:28 +0000

    lmao fix again

--- ptwrdhn2/Lab13/filter.cpp
+++ ptwrdhn2/Lab13/filter.cpp
@@ -27,19 +27,19 @@ filter_fusion(pixel_t **image1, pixel_t **image2) {
 void
 filter_prefetch(pixel_t **image1, pixel_t **image2) {
 	for (int i = 1; i < SIZE - 1; i ++) {
-		__builtin_prefetch(image1[i + 16], 0, 0);
-		__builtin_prefetch(image2[i + 16], 1, 0);
+		__builtin_prefetch(image1[i + 16], 0, 3);
+		__builtin_prefetch(image2[i + 16], 1, 3);
 		filter1(image1, image2, i);
 	}
 
 	for (int i = 2; i < SIZE - 2; i ++) {
-		__builtin_prefetch(image1[i + 16], 0, 0);
-		__builtin_prefetch(image2[i + 16], 1, 0);
+		__builtin_prefetch(image1[i + 16], 0, 3);
+		__builtin_prefetch(image2[i + 16], 1, 3);
 		filter2(image1, image2, i);
 	}
 
 	for (int i = 1; i < SIZE - 5; i ++) {
-		__builtin_prefetch(image2[i + 16], 1, 0);
+		__builtin_prefetch(image2[i + 16], 1, 3);
 		filter3(image2, i);
 	}
 
@@ -60,8 +60,8 @@ filter_all(pixel_t **image1, pixel_t **image2) {
 	// 	filter3(image2, i);
 	// }
 	for (int i = 1; i < SIZE - 1; i ++) {
-		__builtin_prefetch(image1[i + 16], 0, 0);
-		__builtin_prefetch(image2[i + 16], 1, 0);
+		__builtin_prefetch(image1[i + 16], 0, 3);
+		__builtin_prefetch(image2[i + 16], 1, 3);
 		filter1(image1, image2, i);
 		if(i < SIZE - 5) filter3(image2, i);
 		if(i >= 2 && i < SIZE - 2) filter2(image1, image2, i);
