c408388cef0a69f999ede04398c9423ebfa3c1f8
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-04-25 23:39:39 +0000

    eh

--- ptwrdhn2/Lab14/mv-mult.cpp
+++ ptwrdhn2/Lab14/mv-mult.cpp
@@ -12,16 +12,27 @@
 // with the vector, and doing this for each row in the matrix
 
 // vectorize the below code using SIMD intrinsics
-float *mv_mult_vector(float mat[SIZE][SIZE], float vec[SIZE]) {
-    static float ret[SIZE];
+float *
+mv_mult_vector(float mat[SIZE][SIZE], float vec[SIZE]) {
+  static float ret[SIZE], temp[4];
+  int i = 0;
+  __m128 acc, X, Y;
 
-    for (int i = 0; i < SIZE; i ++) {
-        ret[i] = 0;
-        for (int j = 0; j < SIZE; j ++) {
-            // ret[i] += mat[i][j] * vec[j];
-			ret[i] += _mm_mul_ps(mat[i][j], vec[j]);
-        }
+  acc = _mm_set1_ps(0.0);
+  for (; i < SIZE; i+=1) {
+    ret[i] = 0;
+    int j = 0;
+    for (; j < (SIZE-3); j+=4) {
+      Y = _mm_loadu_ps(&vec[j]);
+      X = _mm_loadu_ps(&mat[i][j]);
+      acc = _mm_mul_ps(acc, _mm_mul_ps(X, Y));
+      _mm_storeu_ps(temp, acc);
+      ret[i] += temp[0] + temp[1] + temp[2] + temp[3];
     }
+    for(; j < SIZE; j++) {
+      ret[i] += mat[i][j] * vec[j];
+    }
+  }
 
-    return ret;
+  return ret;
 }
