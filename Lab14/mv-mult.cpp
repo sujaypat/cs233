0c9e9bc895127370f1c675b623779ab820d16c9e
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-04-25 23:48:57 +0000

    lmao

--- Lab14/mv-mult.cpp
+++ Lab14/mv-mult.cpp
@@ -4,7 +4,7 @@
 // Matrix-Vector multiplication
 // mat is a SIZE by SIZE matrix, that is arranged in row-column, format,
 // That is, you first select a particular row, and then a particular column.
-// Each row is laid out as a one-dimensional, array, so if you wanted
+// Each row is laid out as a one-dimensional array, so if you wanted
 // to select a particular row, you would use mat[row].  You can
 // also select smaller intervals, by using &mat[row][col].
 // The vector is also laid out as a one-dimensional arrow, similar to a row.
@@ -12,19 +12,18 @@
 // with the vector, and doing this for each row in the matrix
 
 // vectorize the below code using SIMD intrinsics
-float *
-mv_mult_vector(float mat[SIZE][SIZE], float vec[SIZE]) {
+float * mv_mult_vector(float mat[SIZE][SIZE], float vec[SIZE]) {
   static float ret[SIZE], temp[4];
-  int i = 0;
   __m128 acc, X, Y;
 
   acc = _mm_set1_ps(0.0);
-  for (; i < SIZE; i+=1) {
+  for (int i = 0; i < SIZE; i ++) {
     ret[i] = 0;
-    int j = 0;
-    for (; j < (SIZE-3); j+=4) {
+	int j = 0;
+    for (; j < (SIZE - 3); j += 4) {
       Y = _mm_loadu_ps(&vec[j]);
       X = _mm_loadu_ps(&mat[i][j]);
+
       acc = _mm_mul_ps(acc, _mm_mul_ps(X, Y));
       _mm_storeu_ps(temp, acc);
       ret[i] += temp[0] + temp[1] + temp[2] + temp[3];
