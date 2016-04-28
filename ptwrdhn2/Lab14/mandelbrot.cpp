c2f45fe7168850bb6b7d5eb063e1818b9841faec
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-04-28 03:18:30 +0000

    idk anymore

--- ptwrdhn2/Lab14/mandelbrot.cpp
+++ ptwrdhn2/Lab14/mandelbrot.cpp
@@ -13,30 +13,34 @@ int *cubic_mandelbrot_vector(float x[SIZE], float y[SIZE]) {
     float x1, y1, x2, y2;
 
     for (int i = 0; i < SIZE; i ++) {
-        x1 = y1 = _mm;
+        x1 = y1 = _mm_loadu_ps(0.0);
 
         // Run M_ITER iterations
         for (int j = 0; j < M_ITER; j ++) {
             // Calculate x1^2 and y1^2
-            float x1_squared = x1 * x1;
-            float y1_squared = y1 * y1;
+            float x1_squared = _mm_mul_ps(x1, x1);
+            float y1_squared = _mm_mul_ps(y1, y1);
 
             // Calculate the real piece of (x1 + (y1*i))^3 + (x + (y*i))
-            x2 = x1 * (x1_squared - 3 * y1_squared) + x[i];
+            x2 = _mm_mul_ps(x1, _mm_add_ps( _mm_sub_ps(x1_squared, _mm_mul_ps(3, y1_squared)), x[i]));
 
             // Calculate the imaginary portion of (x1 + (y1*i))^3 + (x + (y*i))
-            y2 = y1 * (3 * x1_squared - y1_squared) + y[i];
+            // y2 = y1 * (3 * x1_squared - y1_squared) + y[i];
+			y2 = _mm_mul_ps(y1, _mm_add_ps( _mm_sub_ps(_mm_mul_ps(3, y1_squared), x1_squared), y[i]));
 
             // Use the resulting complex number as the input for the next
             // iteration
-            x1 = x2;
-            y1 = y2;
+            // x1 = x2;
+			_mm_storeu_ps(x2, x1);
+			_mm_storeu_ps(y2, y1);
+            // y1 = y2;
         }
 
         // calculate the magnitude of the result;
         // we could take the square root, but we instead just
         // compare squares
-        ret[i] = ((x2 * x2) + (y2 * y2)) < (M_MAG * M_MAG);
+        // ret[i] = ((x2 * x2) + (y2 * y2)) < (M_MAG * M_MAG);
+		ret[i] = _mm_add_ps(_mm_mul_ps(x2, x2), _mm_mul_ps(y2, y2)) < _mm_mul_ps(M_MAG, M_MAG)
     }
 
     return ret;
