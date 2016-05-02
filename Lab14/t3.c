412ab747d97ac7357dbd80180957d3a5b6012369
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2016-05-02 00:10:37 +0000

    undid some changes

--- Lab14/t3.c
+++ Lab14/t3.c
@@ -1,15 +1,15 @@
-// A, A
+// A
 #include "declarations.h"
 
 #pragma auto_inline(off)
-void t3(float A[512][512]) {
-    for (int nl = 0; nl < 1000; nl ++) {
-      //      #pragma vector always
-        for (int i = 0; i < 511; i ++) {
-	  for (int j = 0; j < 511; j ++) {
-                A[i + 1][j + 1] = A[i][j + 1] + A[i + 1][j];
-            }
-        }
-        A[0][0] ++;
-    }
+void
+t3(float A[512][512]) {
+	for (int nl = 0; nl < 1000; nl ++) {
+		for (int i = 1; i < 512; i ++) {
+			for (int j = 1; j < 512; j ++) {
+				A[i][j] = A[i-1][j] + A[i][j-1];
+			}
+		}
+		A[0][0] ++;
+	}
 }
