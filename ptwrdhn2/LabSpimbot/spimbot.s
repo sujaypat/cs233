ee65f8f3452f4f53c30d03c006233163370eecd0
ptwrdhn2 <ptwrdhn2@8fdaf7b1-ad66-4706-91fd-677acb01a165>
2016-05-03 22:56:08 +0000

    final commit lmao

--- ptwrdhn2/LabSpimbot/spimbot.s
+++ ptwrdhn2/LabSpimbot/spimbot.s
@@ -68,34 +68,33 @@ str_memory: .space 4096
 
 .text
 main:
+	li    $t0, 100000
+	sw    $t0, TIMER
+
 	li	$s7, 4
 	li	$s8, 8
-	li		$t4, CLOUD_CHANGE_STATUS_INT_MASK		# timer interrupt enable bit
-	or		$t4, $t4, 1		# global interrupt enable
-	or		$t4, $t4, OUT_OF_WATER_INT_MASK		# timer interrupt enable bit
-	or		$t4, $t4, PLANT_FULLY_WATERED_INT_MASK		# timer interrupt enable bit
-#	or		$t4, $t4, REQUEST_PUZZLE_INT_MASK		# timer interrupt enable bit
-	or		$t4, $t4, BONK_MASK
-	mtc0	$t4, $12		# set interrupt mask (Status register)
+	li    	$t4, CLOUD_CHANGE_STATUS_INT_MASK    	# timer interrupt enable bit
+	or    	$t4, $t4, 1    	# global interrupt enable
+	or    	$t4, $t4, OUT_OF_WATER_INT_MASK    	# timer interrupt enable bit
+	or    	$t4, $t4, PLANT_FULLY_WATERED_INT_MASK    	# timer interrupt enable bit
+#   or    	$t4, $t4, REQUEST_PUZZLE_INT_MASK    	# timer interrupt enable bit
+	or   	 $t4, $t4, TIMER_MASK
+	or    	$t4, $t4, BONK_MASK
+	mtc0	$t4, $12    	# set interrupt mask (Status register)
 main_loop:
 	# go wild
 	# the world is your oyster :)
+	la    	$t0, cloud_data
+	sw    	$t0, CLOUD_SCAN
 
-	li		$t0, 10
-	sw  		$t0, VELOCITY
+	la    	$t0, plant_data
+	sw    	$t0, PLANT_SCAN
 
-	la		$t0, cloud_data
-	sw		$t0, CLOUD_SCAN
+	lw    	$s0, BOT_X    	# spimbot x loc
+	lw    	$s1, BOT_Y    	# spimbot y loc
 
-	la		$t0, plant_data
-	sw		$t0, PLANT_SCAN
-
-	lw		$s0, BOT_X		# spimbot x loc
-	lw		$s1, BOT_Y		# spimbot y loc
-
-
-	la		$a0, cloud_data
-	jal		closest_cloud
+	la    	$a0, cloud_data
+	jal    	closest_cloud
 
 	add	$v1, $v1, 18
 	li	$t0, 1
@@ -113,8 +112,8 @@ turn:
 	sw	$0, ANGLE_CONTROL
 	bne	$t3, 180, turn
 move_down:
-	lw	$t0, BOT_Y
-	blt	$t0, $v1, move_down
+	lw	$s1, BOT_Y
+	blt	$s1, $v1, move_down
 
 	move 	$a0, $v0
 	move 	$a1, $v1
@@ -127,8 +126,8 @@ move_down:
 	sw	$t0, ANGLE_CONTROL
 loop:
 	add	$t9, $t9, 1
-	beq	$t9, 7000, leave_loop
-	lw	$s1, BOT_Y
+	beq	$t9, 7500, leave_loop
+	lw 	$s1, BOT_Y
 	blt	$s1, 20, leave_loop
 	j	loop
 leave_loop:
@@ -140,8 +139,8 @@ leave_loop:
 # @param: $a0 = destination.x, $a1 = destination.y
 # points spimbot in the direction of x,y
 change_direction:
-	lw	$s0, BOT_X		# spimbot x loc
-	lw	$s1, BOT_Y		# spimbot y loc
+	lw	$s0, BOT_X    	# spimbot x loc
+	lw	$s1, BOT_Y    	# spimbot y loc
 	move	$s2, $a0
 	move	$s3, $a1
 	sw	$ra, 0($sp)
@@ -152,10 +151,8 @@ change_direction:
 	li	$t0, 1
 	sw	$t0, ANGLE_CONTROL
 dir_loop:
-	lw	$s0, BOT_X		# spimbot x loc
-	lw	$s1, BOT_Y		# spimbot y loc
-	li	$t0, 10
-	sw	$t0, VELOCITY
+	lw	$s0, BOT_X    	# spimbot x loc
+	lw	$s1, BOT_Y    	# spimbot y loc
 	sub	$t0, $s0, $s2
 	sub	$t1, $s1, $s3
 	bgt	$t0, 1, dir_loop
@@ -170,7 +167,7 @@ dir_loop:
 
 
 
-closest_cloud:		#address takes array of cloud data returns x, y pos of nearest cloud
+closest_cloud:    	#address takes array of cloud data returns x, y pos of nearest cloud
 	add	$a0, $a0, $s8
 	lw	$v0, 0($a0)
 	lw	$v1, 4($a0)
@@ -199,7 +196,7 @@ F180:	.float  180.0
 # -----------------------------------------------------------------------
 
 sb_arctan:
-	li	$v0, 0		# angle = 0;
+	li	$v0, 0    	# angle = 0;
 
 	abs	$t0, $a0	# get absolute values
 	abs	$t1, $a1
@@ -209,7 +206,7 @@ sb_arctan:
 	move	$t0, $a1	# int temp = y;
 	neg	$a1, $a0	# y = -x;
 	move	$a0, $t0	# x = temp;
-	li	$v0, 90		# angle = 90;
+	li	$v0, 90    	# angle = 90;
 
 no_TURN_90:
 	bgez	$a0, pos_x 	# skip if (x >= 0)
@@ -236,7 +233,7 @@ pos_x:
 	div.s 	$f5, $f4, $f5	# v^^5/5
 	add.s	$f6, $f6, $f5	# value = v - v^^3/3 + v^^5/5
 
-	l.s	$f8, PI		# load PI
+	l.s	$f8, PI    	# load PI
 	div.s	$f6, $f6, $f8	# value / PI
 	l.s	$f7, F180	# load 180.0
 	mul.s	$f6, $f6, $f7	# 180.0 * value / PI
@@ -256,7 +253,7 @@ pos_x:
 ###
 ###
 
-.kdata				# interrupt handler data (separated just for readability)
+.kdata            	# interrupt handler data (separated just for readability)
 chunkIH:	.space 8	# space for two registers
 non_intrpt_str:	.asciiz "Non-interrupt exception\n"
 unhandled_str:	.asciiz "Unhandled interrupt type\n"
@@ -265,15 +262,15 @@ unhandled_str:	.asciiz "Unhandled interrupt type\n"
 .ktext 0x80000180
 interrupt_handler:
 .set noat
-	move	$k1, $at		# Save $at
+	move	$k1, $at    	# Save $at
 .set at
 	la	$k0, chunkIH
-	sw	$a0, 0($k0)		# Get some free registers
-	sw	$a1, 4($k0)		# by storing them to a global variable
+	sw	$a0, 0($k0)    	# Get some free registers
+	sw	$a1, 4($k0)    	# by storing them to a global variable
 
-interrupt_dispatch:			# Interrupt:
-	mfc0	$k0, $13		# Get Cause register, again
-	beq	$k0, 0, done		# handled all outstanding interrupts
+interrupt_dispatch:        	# Interrupt:
+	mfc0	$k0, $13    	# Get Cause register, again
+	beq	$k0, 0, done    	# handled all outstanding interrupts
 
 	and	$a0, $k0, CLOUD_CHANGE_STATUS_INT_MASK	# is there a cloud status interrupt?
 	bne	$a0, 0, cloud_interrupt
@@ -284,16 +281,28 @@ interrupt_dispatch:			# Interrupt:
 	and	$a0, $k0, PLANT_FULLY_WATERED_INT_MASK
 	bne	$a0, 0, plant_interrupt
 
+	and   	$a0, $k0, TIMER_MASK
+	bne   	$a0, 0, timer_interrupt
+
 	li	$v0, PRINT_STRING	# Unhandled interrupt types
 	la	$a0, unhandled_str
 	syscall
 	j	done
 
+timer_interrupt:
+    sw    $a1, TIMER_ACK
+    li    $t0, 10
+    sw    $t0, VELOCITY
+	lw    $t0, TIMER
+	add    $t0, $t0, 100000
+	sw    $t0, TIMER
+    j    done
+
 bonk_interrupt:
-		sw	$a1, BONK_ACK
+    	sw	$a1, BONK_ACK
 	lw	$s0, BOT_X
 	lw	$s1, BOT_Y
-	bgt	$s1, 150, lowerhalf
+	bgt	$s1, 50, lowerhalf
 	bgt	$s0, 150, topright
 	j	topleft
 lowerhalf:
@@ -306,10 +315,10 @@ topright:
 	li	$t0, 135
 	j	bonk_end
 lowerleft:
-	li	$t0, -10
+	li	$t0, -25
 	j	bonk_end
 lowerright:
-	li	$t0, 190
+	li	$t0, 205
 bonk_end:
 	sw	$t0, ANGLE
 	li	$t0, 1
@@ -331,6 +340,8 @@ cloud_interrupt:
 	add	$t1, $t1, $s7
 	lw	$t1, 0($t1)
 go:
+	lw      $t2, VELOCITY
+	beq   $t2, $0, bonk_interrupt
 	lw	$s0, BOT_X
 	beq	$s0, $t1, stop
 	blt	$s0, $t1, right
@@ -362,36 +373,36 @@ left:
 
 
 puzzle_interrupt:
-	sub 		$sp, $sp, 28
-sw		$ra, 0($sp)
-	sw		$s0, 4($sp)
-sw		$s1, 8($sp)
-sw		$s2, 12($sp)
-	sw		$s3, 16($sp)
-	sw		$s7, 20($sp)
-	sw		$s8, 24($sp)
+	sub     	$sp, $sp, 28
+sw    	$ra, 0($sp)
+	sw    	$s0, 4($sp)
+sw    	$s1, 8($sp)
+sw    	$s2, 12($sp)
+	sw    	$s3, 16($sp)
+	sw    	$s7, 20($sp)
+	sw    	$s8, 24($sp)
 
 
-	sw		$a1, CLOUD_CHANGE_STATUS_ACK
+	sw    	$a1, CLOUD_CHANGE_STATUS_ACK
 
-	la		$a2, puzzle_string
-	sw		$a2, REQUEST_PUZZLE_STRING
+	la    	$a2, puzzle_string
+	sw    	$a2, REQUEST_PUZZLE_STRING
 
-	la		$a0, soln_data
-	la		$a1, puzzle_string
-	la		$a2, puzzle_data
-		jal split_string
-	la		$a0, soln_data
-	sw		$a0, SUBMIT_SOLUTION
-	lw 		$ra, 0($sp)
-lw		$ra, 0($sp)
-	lw		$s0, 4($sp)
-lw		$s1, 8($sp)
-lw		$s2, 12($sp)
-	lw		$s3, 16($sp)
-	lw		$s7, 20($sp)
-	lw		$s8, 24($sp)
-	add		$sp, $sp, 28
+	la    	$a0, soln_data
+	la    	$a1, puzzle_string
+	la    	$a2, puzzle_data
+    	jal split_string
+	la    	$a0, soln_data
+	sw    	$a0, SUBMIT_SOLUTION
+	lw     	$ra, 0($sp)
+lw    	$ra, 0($sp)
+	lw    	$s0, 4($sp)
+lw    	$s1, 8($sp)
+lw    	$s2, 12($sp)
+	lw    	$s3, 16($sp)
+	lw    	$s7, 20($sp)
+	lw    	$s8, 24($sp)
+	add    	$sp, $sp, 28
 	j done
 
 	#Beginning of code copied from SVN to solve puzzle
@@ -414,11 +425,11 @@ split_string:
 ss_recurse:
 	sub	$sp, $sp, 24
 	sw	$ra, 0($sp)
-	sw	$s0, 4($sp)		# char **solution
-	sw	$s1, 8($sp)		# char *str
-	sw	$s2, 12($sp)		# dictionary *dict
-	sw	$s3, 16($sp)		# char *ptr
-	sw	$s4, 20($sp)		# char *prefix
+	sw	$s0, 4($sp)    	# char **solution
+	sw	$s1, 8($sp)    	# char *str
+	sw	$s2, 12($sp)    	# dictionary *dict
+	sw	$s3, 16($sp)    	# char *ptr
+	sw	$s4, 20($sp)    	# char *prefix
 
 	move	$s0, $a0
 	move	$s1, $a1
@@ -426,37 +437,37 @@ ss_recurse:
 	move	$s3, $a1
 
 ss_for:
-	lb	$t0, 0($s3)	        # *ptr
+	lb	$t0, 0($s3)        	# *ptr
 	beq	$t0, $0, ss_done	# *ptr != 0
 
-	move	$a0, $s1		# str
-	add	$a1, $s3, 1		# ptr + 1
-	sub	$a1, $a1, $s1		# ptr + 1 - str
-	jal	sub_str			# sub_str(str, ptr + 1 - str)
-	move	$s4, $v0		# prefix
+	move	$a0, $s1    	# str
+	add	$a1, $s3, 1    	# ptr + 1
+	sub	$a1, $a1, $s1    	# ptr + 1 - str
+	jal	sub_str        	# sub_str(str, ptr + 1 - str)
+	move	$s4, $v0    	# prefix
 
-	move	$a0, $s4		# prefix
-	add	$a1, $s2, 4		# dict->words
-	lw	$a2, 0($s2)		# dict->size
-	jal	in_dict			# in_dict(prefix, dict->words, dict->size)
+	move	$a0, $s4    	# prefix
+	add	$a1, $s2, 4    	# dict->words
+	lw	$a2, 0($s2)    	# dict->size
+	jal	in_dict        	# in_dict(prefix, dict->words, dict->size)
 	beq	$v0, $0, ss_continue
 
-	add	$a0, $s0, 4		# solution + 1
-	add	$a1, $s3, 1		# ptr + 1
-	move	$a2, $s2		# dict
-	jal	split_string		# split_string(solution + 1, ptr + 1, dict)
+	add	$a0, $s0, 4    	# solution + 1
+	add	$a1, $s3, 1    	# ptr + 1
+	move	$a2, $s2    	# dict
+	jal	split_string    	# split_string(solution + 1, ptr + 1, dict)
 	beq	$v0, $0, ss_continue
 
-	sw	$s4, 0($s0)		# *solution = prefix
-	li	$v0, 1			# return 1
+	sw	$s4, 0($s0)    	# *solution = prefix
+	li	$v0, 1        	# return 1
 	j	ss_return
 
 ss_continue:
-	add	$s3, $s3, 1		# ptr++
+	add	$s3, $s3, 1    	# ptr++
 	j	ss_for
 
 ss_done:
-	li	$v0, 0			# return 0
+	li	$v0, 0        	# return 0
 
 ss_return:
 	lw	$ra, 0($sp)
@@ -474,28 +485,28 @@ sub_str:
 	sw	$s0, 4($sp)
 	sw	$s1, 8($sp)
 
-	move	$s0, $a0		# str
-	move	$s1, $a1		# n
+	move	$s0, $a0    	# str
+	move	$s1, $a1    	# n
 
-	add	$a0, $s1, 1		# n + 1
-	jal	malloc			# malloc(n + 1)
-	li	$t0, 0			# len = 0
+	add	$a0, $s1, 1    	# n + 1
+	jal	malloc        	# malloc(n + 1)
+	li	$t0, 0        	# len = 0
 
 sub_str_for:
 	bge	$t0, $s1, sub_str_ret	# len >= n
-	add	$t1, $s0, $t0		# &str[len]
-	lb	$t1, 0($t1)		# str[len]
+	add	$t1, $s0, $t0    	# &str[len]
+	lb	$t1, 0($t1)    	# str[len]
 	beq	$t1, 0, sub_str_ret	# str[len] == '\0'
 
-	add	$t2, $v0, $t0		# &newstr[len]
-	sb	$t1, 0($t2)		# newstr[len] = str[len]
+	add	$t2, $v0, $t0    	# &newstr[len]
+	sb	$t1, 0($t2)    	# newstr[len] = str[len]
 
-	add	$t0, $t0, 1		# len++
+	add	$t0, $t0, 1    	# len++
 	j	sub_str_for
 
 sub_str_ret:
-	add	$t2, $v0, $t0		# &newstr[len]
-	sb	$0, 0($t2)		# newstr[len] = '\0'
+	add	$t2, $v0, $t0    	# &newstr[len]
+	sb	$0, 0($t2)    	# newstr[len] = '\0'
 
 	lw	$ra, 0($sp)
 	lw	$s0, 4($sp)
@@ -506,69 +517,69 @@ sub_str_ret:
 in_dict:
 	sub	$sp, $sp, 20
 	sw	$ra, 0($sp)
-	sw	$s0, 4($sp)		# char *str
-	sw	$s1, 8($sp)		# char **dict
-	sw	$s2, 12($sp)		# int dict_size
-	sw	$s7, 16($sp)		# int i
+	sw	$s0, 4($sp)    	# char *str
+	sw	$s1, 8($sp)    	# char **dict
+	sw	$s2, 12($sp)    	# int dict_size
+	sw	$s7, 16($sp)    	# int i
 
 	move	$s0, $a0
 	move	$s1, $a1
 	move	$s2, $a2
-	move	$s7, $0			# i = 0
+	move	$s7, $0        	# i = 0
 
 id_for:
 	bge	$s7, $s2, id_done 	# i < dict_size
 	mul	$t0, $s7, 4
-	add	$t0, $t0, $s1		# $t0 = &dict[i]
-	move	$a0, $s0		# str
-	lw	$a1, 0($t0)		# dict[i]
-	jal	str_cmp			# str_cmp(str, dict[i])
+	add	$t0, $t0, $s1    	# $t0 = &dict[i]
+	move	$a0, $s0    	# str
+	lw	$a1, 0($t0)    	# dict[i]
+	jal	str_cmp        	# str_cmp(str, dict[i])
 	beq	$v0, $0, id_ret_1	# if (!str_cmp(str, dict[i]))
-	add	$s7, $s7, 1		# i++
+	add	$s7, $s7, 1    	# i++
 	j	id_for
 
 id_ret_1:
-	li	$v0, 1			# return 1
+	li	$v0, 1        	# return 1
 	j	id_ret
 
 id_done:
-	move	$v0, $0			# return 0
+	move	$v0, $0        	# return 0
 id_ret:
 	lw	$ra, 0($sp)
-	lw	$s0, 4($sp)		# char *str
-	lw	$s1, 8($sp)		# char **dict
-	lw	$s2, 12($sp)		# int dict_size
-	lw	$s7, 16($sp)		# int i
+	lw	$s0, 4($sp)    	# char *str
+	lw	$s1, 8($sp)    	# char **dict
+	lw	$s2, 12($sp)    	# int dict_size
+	lw	$s7, 16($sp)    	# int i
 	add	$sp, $sp, 20
 	jr	$ra
 
 
 str_cmp:
-	lb	$t0, 0($a0)		# *src
-	lb	$t1, 0($a1)		# *tgt
+	lb	$t0, 0($a0)    	# *src
+	lb	$t1, 0($a1)    	# *tgt
 	beq	$t0, $0, sc_done	# *srg != 0
 	beq	$t1, $0, sc_done	# *tgt != 0
 	bne	$t0, $t1, sc_done	# *src != *tgt
-	add	$a0, $a0, 1		# src++
-	add	$a1, $a1, 1		# tgt++
+	add	$a0, $a0, 1    	# src++
+	add	$a1, $a1, 1    	# tgt++
 	j	str_cmp
 
 sc_done:
 	bne	$t0, $t1, sc_true	# *src != *tgt
-	move	$v0, $0			# return false
+	move	$v0, $0        	# return false
 	jr	$ra
 
 sc_true:
-	li	$v0, 1			# return true
+	li	$v0, 1        	# return true
 	jr	$ra
 
 
 
 done:
 	la	$k0, chunkIH
-	lw	$a0, 0($k0)		# Restore saved registers
+	lw	$a0, 0($k0)    	# Restore saved registers
 	lw	$a1, 4($k0)
 .set noat
-	move	$at, $k1		# Restore $at
+	move	$at, $k1    	# Restore $at
 .set at
 	eret
